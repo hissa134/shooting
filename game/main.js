d2bdffd4f623e3413d7f044ed56f19f2a23d91fa
hissa134 <mcp-740-cvr@e-net.nara.jp>
2025-09-22 06:14:17 +0000

    球を撃つ

--- game/main.js
+++ game/main.js
@@ -1,45 +1,76 @@
-document.getElementById("shirushi").innerText = "これはゲームです";
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const player = {
+    x: canvas.width / 2 - 15,
+    y: canvas.height - 60,
+    width: 30,
+    height: 30,
+    color: "blue",
+    life: 3,
+};
+
+const bullets = [];
+const BULLET_SPEED = -5 ;
+
+function tryShoot() {
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        width:  5,
+        height: 5,
+        vy: BULLET_SPEED,
+    }) 
+}
 
 //fillRect(x座標, y座標, 横幅, 縦幅)
 
 //自分のキャラクターに見立てた四角形を作る
 
 // width="480", height="640"
-let x = 225;
-let tama = 0;
     
 
 window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowLeft") {
-        x -= 10;
+        if (player.x > 10){
+            player.x -= 10;
+        }
     } else if (e.key === "ArrowRight") {
-        x += 10;
-    } else if (e.key === "Space") {
-        tama += 1;
+        if(player.x < canvas.width - player.width - 10){
+            player.x += 10;
+        }
+    } else if (e.code === "Space") {
+        tryShoot();
     }
 });
 
-let y1 = 0;
-let y2 = -150;
+function update() {
+     for(let i = 0; i < bullets.length; i++) {
+    const bullet = bullets[i];
+    bullet.y += bullet.vy;        
+        if (bullet.y < 0) {
+            bullets.splice(i, 1);
+        }
+    }
+}
 
-function gameLoop() {
+function draw() { 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "red";
-    ctx.fillRect(300, y1, 30, 30);
-    y1 += 5;
-    ctx.fillStyle = "red";
-    ctx.fillRect(150, y2, 30, 30);
-    y2 += 5;
-    ctx.fillStyle = "blue";
-    ctx.fillRect(x, 480, 30, 30);
-    if (tama > 0) {
-    ctx.fillStyle = "white";
-        ctx.fillRect(x + 10, 480 - tama * 10, 10, 10);
-    }
+
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+     ctx.fillStyle = "white";
+     for(let i = 0; i < bullets.length; i++) {
+    const bullet = bullets[i];
+    ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+     }
+}
+
+function gameLoop() {
+    update();
+    draw();
     requestAnimationFrame(gameLoop);
 }
 
