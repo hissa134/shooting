73b99e1c4d39578f41b32e3d8ae5f9da8824b396
hissa134 <mcp-740-cvr@e-net.nara.jp>
2025-11-28 05:12:17 +0000

    背景とプレイヤーの画像を追加

--- game/js/main.js
+++ game/js/main.js
@@ -5,6 +5,9 @@ import { handleCollisions } from "./collision.js";
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const mapImage = new Image();
+mapImage.src = "images/map.png";
+
 initPlayer(canvas);
 
 export const bullets = [];
@@ -63,8 +66,8 @@ function update() {
 
 function draw() { 
     ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+ctx.drawImage(mapImage, 0, 0, canvas.width,
+canvas.height);
     drawPlayer(ctx);
 
      ctx.fillStyle = "white";
